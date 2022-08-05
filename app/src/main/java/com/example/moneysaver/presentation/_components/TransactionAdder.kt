c710eeaaa0e540007d3cc4215763ead6b64fe500
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-08-05 11:58:50 +0300

    Add dialog window for transaction account selection

--- app/src/main/java/com/example/moneysaver/presentation/_components/TransactionAdder.kt
+++ app/src/main/java/com/example/moneysaver/presentation/_components/TransactionAdder.kt
@@ -1,9 +1,10 @@
 package com.example.moneysaver.presentation.categories.additional_composes
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -11,22 +12,33 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.moneysaver.data.data_base.test_data.AccountsData
 import com.example.moneysaver.domain.account.Account
 import com.example.moneysaver.domain.category.Category
 import com.example.moneysaver.domain.transaction.Transaction
 import com.example.moneysaver.presentation.categories.CategoriesViewModel
+import com.example.moneysaver.presentation.transactions.additional_composes.BalanceField
+import com.example.moneysaver.presentation.transactions.additional_composes.TransactionItem
+import com.example.moneysaver.ui.theme.dividerColor
 
 @Composable
 fun TransactionAdder(category: Category, viewModel: CategoriesViewModel, closeAdder: ()->Unit) {
     var sumText by remember { mutableStateOf("") }
     var note by remember { mutableStateOf("") }
     val focusManager = LocalFocusManager.current
+
+    val openChoseAccountDialog = remember { mutableStateOf(false) }
+    val transactionAccount = remember { mutableStateOf(AccountsData.accountsList[0]) }
 
     Column(
         modifier = Modifier
@@ -45,10 +57,12 @@ fun TransactionAdder(category: Category, viewModel: CategoriesViewModel, closeAd
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally
+                    .clickable { openChoseAccountDialog.value = true }
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(text = "From account", fontSize = 16.sp)
-                Text(text = "Account", fontSize = 16.sp)
+                Text(text = transactionAccount.value.title, fontSize = 16.sp)
             }
             Divider(
                 modifier = Modifier
@@ -69,7 +83,7 @@ fun TransactionAdder(category: Category, viewModel: CategoriesViewModel, closeAd
         OutlinedTextField(
             value = sumText,
             onValueChange = {
-                sumText = if (it.isEmpty()){
+                sumText = if (it.isEmpty()) {
                     it
                 } else {
                     when (it.toDoubleOrNull()) {
@@ -78,7 +92,10 @@ fun TransactionAdder(category: Category, viewModel: CategoriesViewModel, closeAd
                     }
                 }
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
             placeholder = { Text("Sum") },
             textStyle = LocalTextStyle.current.copy(
@@ -112,22 +129,81 @@ fun TransactionAdder(category: Category, viewModel: CategoriesViewModel, closeAd
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             Button(
                 shape = RoundedCornerShape(30.dp),
-                colors=ButtonDefaults.buttonColors(backgroundColor = Color(0xff43a0e7)),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xff43a0e7)),
                 onClick = {
-                val transactionAccount = Account(title="TestAccount")
-                val transactionNote: String? = if(note!="") note else null
-                val transaction = Transaction(
-                    sum = sumText.toDoubleOrNull()?: 0.0,
-                    category = category,
-                    account = transactionAccount,
-                    note = transactionNote
-                )
-                viewModel.addTransaction(transaction)
-                closeAdder()
-            }) {
+                    val account = transactionAccount.value
+                    val transactionNote: String? = if (note != "") note else null
+                    val transaction = Transaction(
+                        sum = sumText.toDoubleOrNull() ?: 0.0,
+                        category = category,
+                        account = account,
+                        note = transactionNote
+                    )
+                    viewModel.addTransaction(transaction)
+                    closeAdder()
+                }) {
                 Text("OK")
             }
         }
+    }
 
+    ChooseTransactionAccountDialog(openDialog = openChoseAccountDialog, accountList = AccountsData.accountsList, transactionAccount = transactionAccount)
+    
+}
+
+
+@Composable
+fun ChooseTransactionAccountDialog(openDialog: MutableState<Boolean>, accountList: List<Account>, transactionAccount: MutableState<Account>) {
+    if (openDialog.value) {
+        Dialog(
+            onDismissRequest = {
+                openDialog.value = false
+            }
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .height(350.dp)
+                    .width(300.dp)
+                    .background(Color.White)
+            ) {
+                items(
+                    items = accountList,
+                    itemContent = {
+                        Column() {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        transactionAccount.value = it
+                                        openDialog.value = false
+                                    }
+                                    .background(if (it == transactionAccount.value) Color(0xff59aab3) else Color.White)
+                                    .padding(10.dp)
+                                ,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    painter = painterResource(id = it.accountImg),
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Fit,
+                                    modifier = Modifier
+                                        .padding(2.dp)
+                                        .width(55.dp)
+                                        .height(36.dp)
+                                        .clip(RoundedCornerShape(corner = CornerSize(4.dp)))
+                                )
+                                Column() {
+                                    Text(it.title, fontSize = 16.sp)
+                                    val balanceText: String = (if(it.balance>0) "+" else (if(it.balance < 0) "-" else ""))+"$ "+it.balance
+                                    val balanceColor = if(it.balance>0) Color.Green else (if(it.balance < 0) Color.Red else Color.Gray)
+                                    Text(text = balanceText, color = balanceColor, fontSize = 14.sp)
+                                }
+                            }
+                            Divider(modifier = Modifier.background(dividerColor))
+                        }
+                    }
+                )
+            }
+        }
     }
 }
